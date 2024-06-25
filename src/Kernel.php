f56f33ee8f09eb6395821bee967182b0f48d1979
Tomasz Bartusiak <bartusiak.t@gmail.com>
2024-06-25 13:52:53 +0200

    Change app namespace

--- src/Kernel.php
+++ src/Kernel.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App;
+namespace TomaszBartusiakRekrutacjaSmartiveapp;
 
 use Symfony\Bundle\FrameworkBundle\Kernel\MicroKernelTrait;
 use Symfony\Component\HttpKernel\Kernel as BaseKernel;
