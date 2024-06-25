0767dbe332d5c6f617cb26812c1a7eeeea8ae8f4
Tomasz Bartusiak <bartusiak.t@gmail.com>
2024-06-25 13:37:42 +0200

    Initial commit

---
+++ docker/php/php-entrypoint.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+set -e
+
+if [[ -z "$*" ]]; then
+
+    if [[ -f "composer.json" ]]; then
+        echo "Installing dependencies"
+        composer install
+
+    fi
+
+    if [[ -f "bin/console" ]]; then
+       echo "Running migrations"
+        php bin/console doctrine:migrations:migrate --no-interaction
+    fi
+    
+    echo "Running SUPERVISORD"
+    sudo supervisord -c /etc/supervisord.conf
+
+    echo "Running PHP-FPM"
+    exec php-fpm
+else
+    exec "$@"
+fi
